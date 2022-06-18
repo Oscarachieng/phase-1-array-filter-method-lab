8c8fd872e9c9054f8d5d11bed2e3bb888c5da8b7
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-18 21:14:34 +0300

    commit tree 9420569e547d1993294596eac20cffa92c3f1eb1

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+/*function findMatching(name) {
+ 
+    return name === "Bobby" || name === "bobby"
+ 
+}*/
+const findMatching = (names, nameValue) => {
+    const matchNames = [];
+    for (const name of names) {
+        name.localeCompare(nameValue, undefined,{sensitivity : 'accent'})
+        if (name === nameValue || name === nameValue.toLowerCase()) {
+            matchNames.push(name)
+        }
+        
+    }
+    return matchNames
+    }
+ console.log( findMatching(drivers, "Bobby"))
+
+ const fuzzyMatch = (names, nameValue) => {
+    const matchNames = [];
+    for (const name of names) {
+        name.localeCompare(nameValue, undefined,{sensitivity : 'accent'})
+        if (name.charAt(0) === nameValue.charAt(0)) {
+            matchNames.push(name)
+        }
+        
+    }
+    return matchNames
+    }
+    console.log(fuzzyMatch(drivers, "bobby"))
+
+
+    const drivers2 = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+
+
+    const matchName = (items, nameToSearch) => {
+        
+    const match = items.filter(item =>{
+            return item.name === nameToSearch
+        })
+      return match
+    }
+   
+
+    console.log(matchName(drivers2,"Bobby"))
+       
+      
+
+
+
+
+
+  
+
+
